ffa2d53ca11487ee82500df3b63d2c1a7668d6da
Swastik Kakran <kakranswastik2007@gmail.com>
2026-04-01 20:43:41 +0530

    add isDeleted property in userSchema

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -24,6 +24,10 @@ const userSchema = new Schema({
         trim: true,
         required: true
     },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    },
     refreshToken: String
     
 }, {timestamps: true})
